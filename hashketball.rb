e828576c8d107cf75f1209dd40a3581985fb2408
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-25 07:17:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -25,15 +25,16 @@ def game_hash
       [
         {
           player_name: "Alan Anderson",
-          number:
-          shoe:
-          points:
-          rebounds:
-          assists:
-          steals:
-          blocks:
-          slam_dunks:
-        }
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1,
+        },
+        
         ]
     }
   }
