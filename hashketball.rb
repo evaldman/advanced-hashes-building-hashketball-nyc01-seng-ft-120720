e915c600ee2726b9134f341164c43f8c1f26a2ee
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-25 07:10:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,12 +1,14 @@
 def game_hash
   {
     home:{
-      team_name:
-      colors:
-      players:
+      team_name: ""
+      colors:[]
+      players:[{}]
     } 
     away:{
-      
+      team_name: ""
+      colors:[]
+      players:[{}]
     }
   }
   
