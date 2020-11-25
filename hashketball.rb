2c7c9f2c36584196984ed856ef3db6c613347d24
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-25 07:22:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,10 +1,32 @@
 def game_hash
   {
     home:{
-      team_name: ""
-      colors:[]
+     team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
       players:
       [
+        {
+          player_name: "Alan Anderson",
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        }, 
+        {
+          player_name: "Reggie Evand",
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 1,
+          slam_dunks: 15
+        },
         {
           player_name:
           number:
@@ -44,6 +66,17 @@ def game_hash
           steals: 12,
           blocks: 1,
           slam_dunks: 15
+        },
+        {
+          player_name:
+          number:
+          shoe:
+          points:
+          rebounds:
+          assists:
+          steals:
+          blocks:
+          slam_dunks:
         }
         ]
     }
