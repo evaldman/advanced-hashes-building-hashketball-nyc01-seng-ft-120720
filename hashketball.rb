727c682c4fd59962a7fe5d9f574b4115ba143e3b
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-25 07:40:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -92,7 +92,7 @@ def game_hash
           slam_dunks: 10
         },
         {
-          player_name: "DeSagna Diop"
+          player_name: "DeSagna Diop",
           number: 2,
           shoe: 14,
           points: 24,
@@ -114,7 +114,7 @@ def game_hash
           slam_dunks: 0
         },
         {
-          player_name: "Kemba Walker"
+          player_name: "Kemba Walker",
           number: 33,
           shoe: 15,
           points: 6,
